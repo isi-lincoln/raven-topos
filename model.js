21e628ee695d32c2de2f319401ab9c3557c5751a
Lincoln Thurlow <lincoln@isi.edu>
2020-02-11 10:55:00 -0800

    init cockroach
    Signed-off-by: Lincoln Thurlow <lincoln@isi.edu>

---
+++ model.js
@@ -0,0 +1,41 @@
+let toponame = "cockroach"+Math.random().toString().substr(-6);
+
+function server(name) {
+	return {
+		name: name,
+		image: 'debian-buster',
+		cpu: { cores: 2 },
+		memory: { capacity: GB(4) },
+	};
+}
+
+function vertex(name) {
+  return {
+    name: name,
+    image: 'cumulusvx-3.7-mvrf',
+    cpu: { cores: 2 },
+    memory: { capacity: GB(2) },
+  };
+}
+
+//   ---s1----
+//  /  /  \   \
+// a   b   c   d
+topo = {
+	name: toponame,
+	nodes: [
+		server("a"),
+		server("b"),
+		server("c"),
+		server("d"),
+	],
+	switches: [
+		vertex("s1"),
+	],
+	links: [
+		Link('a', 1, 's1', 1),
+		Link('b', 1, 's1', 2),
+		Link('c', 1, 's1', 3),
+		Link('d', 1, 's1', 4),
+	],
+}
